97842cde2aefeca80d8ed558b9c72bcdd85f5ada
Breno Garcia <BrenoGHeat@gmail.com>
2024-05-20 12:56:19 -0300

    portfolio finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import { AboutMeSection } from "./components/AboutMeSection";
+import { BannerSection } from "./components/BannerSection";
+import { ProjectsSection } from "./components/ProjectsSection";
+import { TechSection } from "./components/TechSection";
+import { HomePage } from "./page/HomePage";
 
-import { HomePage } from './page/HomePage';
-import './styles/index.css';
-
+import "./styles/index.css";
 
 function App() {
-
   return (
     <div className="App">
-     <HomePage />
+      <HomePage>
+        <BannerSection />
+        <AboutMeSection />
+        <TechSection />
+        <ProjectsSection />
+      </HomePage>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/page/HomePage/index.jsx
+++ src/page/HomePage/index.jsx
@@ -1,24 +1,16 @@
-import { AboutMeSection } from "../../components/AboutMeSection"
-import { BannerSection } from "../../components/BannerSection"
-import { Footer } from "../../components/Footer"
-import { Header } from "../../components/Header"
-import { ProjectsSection } from "../../components/ProjectsSection"
-import { TechSection } from "../../components/TechSection"
 
-export const HomePage = () => {
-    return(
-        <>
-            <Header />
-        <main>
-            <BannerSection />
-            <AboutMeSection />
-            <TechSection />
-            <ProjectsSection />
-        </main>
-        
-        <Footer />
-        
-        
-        </>
-    )
-}
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+
+
+export const HomePage = ( {children} ) => {
+  return (
+    <>
+      <Header />
+     <main>
+       {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
